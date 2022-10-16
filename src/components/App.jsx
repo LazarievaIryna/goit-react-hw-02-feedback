ac6e09abd2e8a0402f679205a2c676141a5e397b
Iryna Lazareva <il337733@gmail.com>
2022-10-16 20:04:48 +0300

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+  render() {
+    // const { good, neutral, bad } = this.state;
+    return (
+      <>
+        <section>
+          <h2>Please leave feedback</h2>
+          <button type="button">Good</button>
+          <button type="button">neutral</button>
+          <button type="button">bad</button>
+
+          <section>
+            <h2>Statistics</h2>
+            <span>Good:</span>
+            <span>Neutral:</span>
+            <span>Bad:</span>
+          </section>
+        </section>
+      </>
+    );
+  }
+}

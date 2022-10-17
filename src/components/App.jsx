b139ec78fccc49431db31b1f420bc8a955f0a82f
Iryna Lazareva <il337733@gmail.com>
2022-10-17 20:57:19 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,19 +5,31 @@ export class App extends Component {
     neutral: 0,
     bad: 0,
   };
+
+  handleFeedback = () => {
+    this.setState(prevState => {
+      return {
+        good: prevState.good + 1,
+      };
+    });
+  };
+
   render() {
-    // const { good, neutral, bad } = this.state;
+    // const { good } = this.state;
     return (
       <>
         <section>
           <h2>Please leave feedback</h2>
-          <button type="button">Good</button>
+          <button type="button" onClick={this.handleFeedback} name="good">
+            Good
+          </button>
           <button type="button">neutral</button>
           <button type="button">bad</button>
 
           <section>
             <h2>Statistics</h2>
-            <span>Good:</span>
+
+            <span>Good:{this.state.good}</span>
             <span>Neutral:</span>
             <span>Bad:</span>
           </section>
